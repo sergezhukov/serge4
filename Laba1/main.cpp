4e151199d5785357ebd0c2271da9c1b34286b8d9
sergezhukov <sergezhukov@mail.ru>
2016-09-20 19:32:57 +0300

    xz

---
+++ Laba1/main.cpp
@@ -0,0 +1,10 @@
+#include  <iostream>
+void main()
+{
+	int t, u, k = 6, r = 5e-7, x = 0.095;
+	t = tan(x)+r*(1-log(x));
+	u = t/(x^3+1)/(1-exp(k-4));
+	std::cout << "t=" << t;
+	std::cout << "  u=" << u;
+	system("PAUSE");
+}
